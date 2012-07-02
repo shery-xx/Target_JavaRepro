ea8ed8e07bb840354138b203920cfb718a72e563
CodePorting <info@codeporting.com>
2012-07-02 02:33:56 -0700

    Auto port from CodePorting

---
+++ PortedCode/MyTestRun1/SigninToken.java
@@ -0,0 +1,26 @@
+/**
+     * <p>
+     * </p>
+     *//*--------------------------------------------------------------------------------------+
+|   
+|  This sample is developed and maintained by CodePorting venture
+|  [http://codeporting.com ] a division of Aspose Pty Ltd.
+|
++--------------------------------------------------------------------------------------*/
+
+package CodePortingCSharp2Java;
+
+// ********* THIS FILE IS AUTO PORTED FORM C# USING CODEPORTING.COM *********
+
+
+ public class SigninToken
+{
+    /**
+     * <p>
+     * </p>
+     */
+    public static String Token = "";   
+    public String Test2="here";
+    public String Test3 = "text";		
+}
+
