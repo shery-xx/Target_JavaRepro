32ab233f0c17b1ba97c9b49ee28569e8229858be
CodePorting <info@codeporting.com>
2012-07-02 02:43:01 -0700

    Auto port from CodePorting

---
+++ PortedCode/MyTestRun1/CodePortTest.java
@@ -0,0 +1,17 @@
+/*--------------------------------------------------------------------------------------+
+|   
+|  This sample is developed and maintained by CodePorting venture
+|  [http://codeporting.com ] a division of Aspose Pty Ltd.
+|
++--------------------------------------------------------------------------------------*/
+
+package NameSpaceData;
+
+// ********* THIS FILE IS AUTO PORTED FORM C# USING CODEPORTING.COM *********
+
+
+ public class CodePortTest
+{
+    public integer count = 0;
+}
+
